7815a20909c7cd1def212aa9ecb2a24aa5714611
Alexey Khmelev <avhmel@gmail.com>
2019-06-03 18:19:43 +0700

    Implement claim reward

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,14 +5,21 @@ contract Lottery {
   uint256 public ticketPrice;
 
   uint256 public ticketsAvailable;
-  mapping (uint => address) public participants;
+  mapping (uint => address payable) public participants;
 
   event TicketPurchased(address buyer);
+  event TicketsSoldOut(uint256 winnerRevealBlockNumber);
+  event RewardClaimed(address buyer);
+
+  uint256 winnerRevealBlockNumber;
+  bool isRewardClaimed;
 
   constructor(uint256 _ticketsTotal, uint256 _ticketPrice) public payable {
     ticketsTotal = _ticketsTotal;
     ticketsAvailable = _ticketsTotal;
     ticketPrice = _ticketPrice;
+
+    isRewardClaimed = false;
 
     _buyTicket();
   }
@@ -28,5 +35,26 @@ contract Lottery {
     participants[ticketsTotal - ticketsAvailable] = msg.sender;
     ticketsAvailable = ticketsAvailable - 1;
     emit TicketPurchased(msg.sender);
+
+    if (ticketsAvailable == 0) {
+      winnerRevealBlockNumber = block.number + 50;
+      emit TicketsSoldOut(winnerRevealBlockNumber);
+    }
+  }
+
+  function winner() public view returns (address payable winnerAddr) {
+    require(ticketsAvailable == 0, "Ticket has not been sold out");
+    require(block.number > winnerRevealBlockNumber, "Winner has not been revealed yet");
+    winnerAddr = participants[uint256(blockhash(winnerRevealBlockNumber)) % ticketsTotal];
+  }
+
+  function claimReward() public {
+    require(!isRewardClaimed, "Reward already claimed");
+
+    address payable winnerAddr = winner();
+    require(msg.sender == winner(), "Not winner");
+
+    winnerAddr.transfer(ticketsTotal * ticketPrice);
+    isRewardClaimed = true;
   }
 }
