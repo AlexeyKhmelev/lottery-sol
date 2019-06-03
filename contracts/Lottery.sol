68eacf7d4152dca7512a75e1e35e58b77018b02c
Alexey Khmelev <avhmel@gmail.com>
2019-06-03 16:14:52 +0700

    Implement buy ticket method

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,8 +4,29 @@ contract Lottery {
   uint256 public ticketsTotal;
   uint256 public ticketPrice;
 
-  constructor(uint256 _ticketsTotal, uint256 _ticketPrice) public {
+  uint256 public ticketsAvailable;
+  mapping (uint => address) public participants;
+
+  event TicketPurchased(address buyer);
+
+  constructor(uint256 _ticketsTotal, uint256 _ticketPrice) public payable {
     ticketsTotal = _ticketsTotal;
+    ticketsAvailable = _ticketsTotal;
     ticketPrice = _ticketPrice;
+
+    _buyTicket();
+  }
+
+  function buyTicket() public payable {
+    _buyTicket();
+  }
+
+  function _buyTicket() private {
+    require(ticketsAvailable > 0, "No tickets available");
+    require(msg.value == ticketPrice, "Invalid message value");
+
+    participants[ticketsTotal - ticketsAvailable] = msg.sender;
+    ticketsAvailable = ticketsAvailable - 1;
+    emit TicketPurchased(msg.sender);
   }
 }
