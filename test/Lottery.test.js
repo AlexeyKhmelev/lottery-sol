482a201d17fad042b4956a31acde0488de3002b4
Alexey Khmelev <avhmel@gmail.com>
2019-06-03 14:52:07 +0700

    Add test on tickets total/price getters

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,4 +1,24 @@
-contract('Lottery', () => {
-  it('Initial empty test', async () => {
+const Lottery = artifacts.require('Lottery');
+
+contract('Lottery', ([owner]) => {
+  describe('Getters', () => {
+    const ticketsTotal = 50;
+    const ticketPrice = 100;
+
+    let lotteryContract;
+
+    before('Create Lottery Contract', async () => {
+      lotteryContract = await Lottery.new(ticketsTotal, ticketPrice, { from: owner, value: ticketPrice });
+    });
+
+    it('Get Tickets Number', async () => {
+      const actual = await lotteryContract.ticketsTotal();
+      assert.isTrue(actual.eq(new web3.utils.BN(ticketsTotal)));
+    });
+
+    it('Get Ticket Price', async () => {
+      const actual = await lotteryContract.ticketPrice();
+      assert.isTrue(actual.eq(new web3.utils.BN(ticketPrice)));
+    });
   });
 });
